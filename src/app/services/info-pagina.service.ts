661706cd547e158e43c5db9588ecbc1a26ab52bc
Mauricio Torres Ramos <mauricio3389@hotmail.com>
2020-11-04 21:46:15 -0300

    Fin sección 4

---
+++ src/app/services/info-pagina.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { InfoPagina } from 'src/app/interfaces/info-pagina.interface'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InfoPaginaService {
+
+  info: InfoPagina = {};
+  cargada = false;
+
+  constructor( private http: HttpClient ) {
+
+    this.http.get('assets/data/data-pagina.json')
+      .subscribe( (resp: InfoPagina) => {
+        this.cargada = true;
+        this.info = resp;
+      } );
+   }
+}
