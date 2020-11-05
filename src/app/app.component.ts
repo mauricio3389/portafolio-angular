0f16388e2723d223a297c991fa44dc7354e271e2
Mauricio Torres Ramos <mauricio3389@hotmail.com>
2020-11-05 11:52:38 -0300

    Fin de la sección 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { InfoPaginaService } from 'src/app/services/info-pagina.service'
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
+import { ProductosService } from 'src/app/services/productos.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,7 @@ import { InfoPaginaService } from 'src/app/services/info-pagina.service'
 })
 export class AppComponent {
   constructor(
-    serviceInfo: InfoPaginaService
+    serviceInfo: InfoPaginaService,
+    serviceProducto: ProductosService,
    ) { }
 }
--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { InfoPagina } from 'src/app/interfaces/info-pagina.interface'
+import { Equipo } from 'src/app/interfaces/equipo.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,28 @@ import { InfoPagina } from 'src/app/interfaces/info-pagina.interface'
 export class InfoPaginaService {
 
   info: InfoPagina = {};
+  equipo: Equipo[];
   cargada = false;
 
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
 
+   private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-      } );
+    .subscribe( (resp: InfoPagina) => {
+      this.cargada = true;
+      this.info = resp;
+    } );
+   }
+
+   private cargarEquipo() {
+    this.http.get('https://porfolio-b2337.firebaseio.com/equipo.json')
+    .subscribe( (resp: Equipo[]) => {
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(this.equipo);
+    } );
    }
 }
